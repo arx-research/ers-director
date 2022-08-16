224f09d0675679c9de8f4ef5280ee5fe919011b1
Cameron Robertson <charles.c.robertson@gmail.com>
2022-08-16 15:37:21 -0600

    updating readme and redirect.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # HaLo Director
 
-Sends a chip scan to the correct location!
+Direct a chip based on the `chipId` and the desired URI in the ENS registry. If the chip or URI cannot be located then the user will be redirected to `halo.vrfy.ch`.
 
 ## Commands
 
--- javascript/index.js
+++ javascript/index.js
@@ -3,24 +3,28 @@ import URL from "url-parse";
 import { abi } from "./abi.json";
 import parseKeys from "./helpers/parseKeys";
 
+// Canonical ENS registry and network.
+const ENS_REGISTRY = "0x58228adF88fe48E421A8220Ede6CCFE70AB2A785";
+const ENS_NETWORK = "https://kovan.optimism.io";
+
 async function readContract() {
-  // Create Provider
+  // Create Provider.
   const provider = new ethers.providers.JsonRpcProvider(
-    "https://kovan.optimism.io"
+    ENS_NETWORK
   );
 
-  // Create contract instance
+  // Create contract instance.
   const contact = await new ethers.Contract(
-    "0x58228adF88fe48E421A8220Ede6CCFE70AB2A785",
+    ENS_REGISTRY,
     abi,
     provider
   );
 
-  // Run function
+  // Run function.
   const url = new URL(window.location.href, true);
 
   if (!url.query.static) {
-    window.location.href = "https://eth.vrfy.ch";
+    window.location.href = "https://halo.vrfy.ch";
     return;
   } else {
     const chipId = parseKeys(url.query.static);
@@ -29,10 +33,9 @@ async function readContract() {
 
     if (exists) {
       const url = await contact.chipUri(chipId);
-
       window.location.href = url + "?" + params;
     } else {
-      window.location.href = "https://eth.vrfy.ch/" + "?" + params;
+      window.location.href = "https://halo.vrfy.ch/" + "?" + params;
     }
   }
 }
