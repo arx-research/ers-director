ec3048c86d9cbbb916a3e7520348f4dcd93e8af9
bweick <brian.weickmann@gmail.com>
2023-09-05 10:35:20 -0600

    Integrate ERS into halo director.

--- javascript/helpers/parseKeys.js
+++ javascript/helpers/parseKeys.js
@@ -2,13 +2,23 @@ import { ethers } from "ethers";
 
 const formatHex = (bytes) => (bytes.slice(0, 2) == "0x" ? bytes : "0x" + bytes);
 
-export default function parseKeys(payload) {
+export function parseKeys(payload) {
   const pkLength = parseInt("0x" + payload.slice(0, 2)) * 2;
   const pkRaw = payload.slice(2, pkLength + 2);
 
   const key = ethers.utils.keccak256(
     "0x" + ethers.utils.computePublicKey(formatHex(pkRaw)).slice(4)
   );
-
+  console.log(key);
   return key;
+}
+
+export function keysToAddress(payload) {
+  const pkLength = parseInt("0x" + payload.slice(0, 2)) * 2;
+  const pkRaw = payload.slice(2, pkLength + 2);
+
+  const publicKey = ethers.utils.computePublicKey(formatHex(pkRaw)).slice(4);
+  console.log("0x" + publicKey);
+  console.log(ethers.utils.computeAddress(ethers.utils.arrayify(publicKey)));
+  return publicKey
 }
--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,8 @@
 import { ethers } from "ethers";
 import URL from "url-parse";
 import { abi } from "./abi.json";
-import parseKeys from "./helpers/parseKeys";
+import { abi as ersAbi } from "./chipRegistryAbi.json";
+import { parseKeys, keysToAddress } from "./helpers/parseKeys";
 
 function fixUrl(url) {
   let u = url;
@@ -23,6 +24,9 @@ const ENS_REGISTRY = "0xB26A49dAD928C6A045e23f00683e3ee9F65dEB23";
 const ENS_NETWORK =
   "https://opt-mainnet.g.alchemy.com/v2/Mx_q-MkGapjZcN0E6Kh4dJVbZq84F3zG";
 
+const CHIP_REGISTRY = "0x369Ae94A0868a27154b2eB11B6dEb14B2cf0b7f0";
+const ERS_NETWORK = "https://goerli.infura.io/v3/a2fdbec687924872818c9d3cfeb67c9f";
+
 function makeStatic(pk1, pk2, pk3) {
   let out = "41" + pk1;
 
@@ -41,34 +45,93 @@ function makeStatic(pk1, pk2, pk3) {
   return out;
 }
 
-async function readContract() {
+// window.location.href = "https://halo.vrfy.ch/" + "?" + params;
+
+async function checkLegacy(url) {
+  const { pk1, pk2, pk3 } = url.query;
+
   // Create Provider.
   const provider = new ethers.providers.JsonRpcProvider(ENS_NETWORK);
 
   // Create contract instance.
-  const contact = await new ethers.Contract(ENS_REGISTRY, abi, provider);
+  const contract = await new ethers.Contract(ENS_REGISTRY, abi, provider);
 
-  // Run function.
-  const url = new URL(window.location.href, true);
+  const statik = url.query.static || makeStatic(pk1, pk2, pk3);
+  const chipId = parseKeys(statik);
+  const exists = await contract.chipExists(chipId);
+  const params = window.location.href.split("?")[1];
+
+  if (exists) {
+    const url = await contract.chipUri(chipId);
+    const u = fixUrl(url);
+
+    return { exists, "redirectUrl": u + "?" + params };
+  } else {
+    return { exists, "redirectUrl": "https://halo.vrfy.ch" };
+  }
+}
+
+function parseRecordsForContentApp(data) {
+  let arrayOfRecords;
+  const encoder = new ethers.utils.AbiCoder();
+  try{
+    arrayOfRecords = (encoder.decode(["tuple(bytes32,bytes)[]"], data))[0];
+  } catch (error){
+    throw new Error(`Something went wrong. Check the gateway's response. Call returned: ${data}`);
+  }
+  // Convert the bytes from each record in the array to a string and remove null characters
+  const formattedArrayOfRecords = arrayOfRecords.map((recordArray) => {return {recordType: ethers.utils.toUtf8String(recordArray[0]).replace(/\u0000/g, ''), content: ethers.utils.toUtf8String(recordArray[1]).replace(/\u0000/g, '')}; });
+  formattedArrayOfRecords.filter((record) => record.recordType === "contentApp");
+
+  return formattedArrayOfRecords[0].content;
+}
+
+async function checkERS(url) {
+  // 0x1Be84c8Fdd9d945AeE2fc66CF0B2809d2C8f36f4 <-- chip has been claimed on Goerli
   const { pk1, pk2, pk3 } = url.query;
 
+  // Create Provider.
+  const provider = new ethers.providers.JsonRpcProvider(ERS_NETWORK);
+
+  // Create contract instance.
+  const chipRegistry = await new ethers.Contract(CHIP_REGISTRY, ersAbi, provider);
+
+  const statik = url.query.static || makeStatic(pk1, pk2, pk3);
+  const chipId = "0x" + parseKeys(statik).slice(26);     // remove 0x and 12 bytes from computed public key
+  console.log(chipId);
+  try {
+    let blockTag = "latest";
+    const data = await provider.call({
+      to: chipRegistry.address,
+      ccipReadEnabled: true,
+      data: chipRegistry.interface.encodeFunctionData("resolveChipId", [chipId]),
+    }, blockTag);
+
+    return parseRecordsForContentApp(data);
+  } catch(e) {
+    throw e;
+  }
+}
+
+async function readContract() {
+  // Run function.
+  const url = new URL(window.location.href, true);
+
+  const { pk1, pk2, pk3 } = url.query;
+  
   // No params
   if (!url.query.static && !pk1) {
     window.location.href = "https://halo.vrfy.ch";
     return;
   } else {
-    const statik = url.query.static || makeStatic(pk1, pk2, pk3);
-    const chipId = parseKeys(statik);
-    const exists = await contact.chipExists(chipId);
-    const params = window.location.href.split("?")[1];
+    const legacy = await checkLegacy(url);
 
-    if (exists) {
-      const url = await contact.chipUri(chipId);
-      const u = fixUrl(url);
-
-      window.location.href = u + "?" + params;
+    if (legacy.exists) {
+      window.location.href = legacy.redirectUrl;
+      return;
     } else {
-      window.location.href = "https://halo.vrfy.ch/" + "?" + params;
+      const contentApp = await checkERS(url);
+      window.location.href = contentApp;
     }
   }
 }
